21487934703f1959916a322fd06b121f92b11a8d
Peerapat <peerapat.chanc@ku.th>
2025-03-12 17:45:19 +0700

    Login and Signup

--- app/src/main/java/com/example/veryclean/MainActivity.kt
+++ app/src/main/java/com/example/veryclean/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.veryclean
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.veryclean.databinding.ActivityMainBinding
+import com.example.veryclean.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment()) // โหลด HomeFragment
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,5 +37,10 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment) // ใช้ FragmentContainerView
+            .commit()
     }
 }
